f37cda244a4bcf805ceec36b8987199e210adb89
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-11 18:51:41 -0300

    feat:: installed keen-slider to set up a carrousel feature in the index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
+import tee1 from '../assets/shirt01.png';
 import { HomeContainer, Product } from "../styles/pages/home";
 
-import tee1 from '../assets/shirt01.png';
-
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={tee1.src} width={520} height={480} alt={""} />
 
         <footer>
@@ -15,7 +23,25 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={tee1.src} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>T-Shirt X</strong>
+          <span>US$ 35.29</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tee1.src} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>T-Shirt X</strong>
+          <span>US$ 35.29</span>
+        </footer>
+      </Product>
+      
+      <Product className="keen-slider__slide">
         <Image src={tee1.src} width={520} height={480} alt={""} />
 
         <footer>
