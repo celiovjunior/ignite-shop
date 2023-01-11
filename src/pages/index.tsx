3d1d2cc390e6c814645cbf4a86eed6e994bf7a8e
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-11 17:02:32 -0300

    dev: imported logo img from figma design and set up global css styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+})
+
 export default function Home() {
   return (
-    <h1>hello</h1>
+    <Button>
+      Enviar
+    </Button>
   )
 }
 
