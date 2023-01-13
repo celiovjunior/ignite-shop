5e0ec189f24e328d01873849439ab67d064ff52f
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-13 08:18:37 -0300

    feat: made the checkout session with stripe api and ran some tests

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,6 +12,7 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
@@ -76,7 +77,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async({ param
           style: 'currency',
           currency: 'USD'
         }).format(price.unit_amount! / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1, // 1 hora
